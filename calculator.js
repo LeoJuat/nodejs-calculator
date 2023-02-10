81438dbf441ecaa89fd34b41144df9726a3de738
leojuat <leonellejuat@gmail.com>
2023-02-09 16:33:43 -0800

    added bmi calculator

--- calculator.js
+++ calculator.js
@@ -1,9 +1,33 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+  const num1 = Number(req.body.num1);
+  const num2 = Number(req.body.num2);
+  const result = num1 + num2;
+
+  res.send("The answer is " + result);
+});
+
+app.get("/bmicalculator", (req, res) => {
+  res.sendFile(__dirname + "/bmi.html");
+});
+
+app.post("/bmicalculator", (req, res) => {
+  const weight = parseFloat(req.body.weight);
+  const height = parseFloat(req.body.height);
+  const result = weight / (height * height);
+
+  res.send("Your bmi is " + result);
 });
 
 app.listen(port, () => {
